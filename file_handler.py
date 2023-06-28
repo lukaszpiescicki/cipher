9a7d407221f21c5993b81db64b06b47bd7ebbb55
Lukasz <lukasz.piescicki@gmail.com>
2023-06-28 17:32:24 +0200

    feat: add save to file

--- file_handler.py
+++ file_handler.py
@@ -1,7 +1,36 @@
-def save_to_file(file_name: str, data_to_save: str):
-    with open(file_name, 'w') as file:
-        file.write(data_to_save)
+import json
+import os
+import io
 
 
+def startup_check(path: str):
+    if os.path.isfile(path) and os.access(path, os.R_OK):
+        return True
+    else:
+        return False
 
 
+def save_to_file(file_name: str, data: dict):
+    path = f"./{file_name}.json"
+    file_status = startup_check(path)
+
+    if file_status:
+        print("istnieje")
+        with open(f"{file_name}.json", "r+") as file:
+            file_data = json.load(file)
+            for k, v in data.items():
+                file_data[k] = v
+            file.seek(0)
+            json.dump(file_data, file, indent=4)
+            file.truncate()
+    else:
+        print("nie istnieje")
+        with open(f"{file_name}.json", "w") as file:
+            json.dump(data, file)
+
+
+x = {"organization": "GeeksForGeeks", "city": "Noida", "country": "India"}
+y = {"pin": 110096}
+
+save_to_file("test", x)
+save_to_file("test", y)
