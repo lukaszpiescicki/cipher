2ba454b33d969f54fae2d87408a079ae77b2b1b9
Lukasz <lukasz.piescicki@gmail.com>
2023-07-13 21:40:57 +0200

    refactor: update .gitignore about src/files dir, remove commented code form main.py

--- main.py
+++ main.py
@@ -1,40 +1,10 @@
-class Cipher:
-    def __init__(self) -> None:
-        pass
-
-    def encrypt(self, text: str, key: int) -> str:
-        result: str = ""
-        for i in range(len(text)):
-            char: str = text[i]
-
-            if char == " ":
-                result += " "
-            elif char.isupper():
-                result += chr((ord(char) + key - 65) % 26 + 65)
-            else:
-                result += chr((ord(char) + key - 97) % 26 + 97)
-        return result
-
-    def decrypt(self, text: str, key: int):
-        result = ""
-
-        for i in range(len(text)):
-            char: str = text[i]
-
-            if char == " ":
-                result += " "
-            elif char.isupper():
-                result += chr((ord(char) - key - 65) % 26 + 65)
-            else:
-                result += chr((ord(char) - key - 97) % 26 + 97)
-        return result
+from src.manager import Manager
 
 
 def main():
-    cp = Cipher()
+    manager = Manager()
+    manager.run()
 
 
 if __name__ == "__main__":
     main()
-
-
